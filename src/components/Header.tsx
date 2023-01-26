5f7a6a2f4b34dbcea05b622146b1541405a9edb5
Gauvin jonathan <59934702+Jonathan97480@users.noreply.github.com>
2023-01-26 19:09:12 +0400

    add components header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,24 @@
-
+import { GiHamburgerMenu } from 'react-icons/gi';
+import Image from 'next/image';
+import { Button } from './ux';
 export default function Header() {
 
     return (
-        <header>
-
+        <header className='header'>
+            <GiHamburgerMenu className='header__burgerIcon' />
+            <div className='header__content'>
+                <Image className='header__content--image' src={require('../images/icons/logo.png')} alt="logo du site" width={47} height={57} />
+                <nav className='header__content--nav'>
+                    <ul className='header__content--ul'>
+                        <li className='header__content--ul__li'>Accueil</li>
+                        <li className='header__content--ul__li'>Qui sommes nous</li>
+                        <li className='header__content--ul__li'>Nos projets</li>
+                        <li className='header__content--ul__li'>Nos méthode de travail</li>
+                        <li className='header__content--ul__li'>Nous contacter</li>
+                    </ul>
+                </nav>
+                <Button text="Connexion" onClick={() => { }} />
+            </div>
         </header>
     )
 }
