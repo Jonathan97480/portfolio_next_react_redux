e5f4f6579d9a02c990692723d6114a40b7ca12a3
Gauvin jonathan <59934702+Jonathan97480@users.noreply.github.com>
2023-01-26 19:07:40 +0400

    add component ux buton

--- src/components/ux/Button.tsx
+++ src/components/ux/Button.tsx
@@ -1,10 +1,36 @@
+import React, { useEffect, useState } from 'react';
+
+interface ButtonProps {
+    text: string;
+    onClick: () => void;
+    disabled?: boolean;
+    inverted?: boolean;
 
 
-export default function Button() {
+}
+
+
+export default function Button({ text, onClick, disabled = false, inverted = false }: ButtonProps) {
+
+    const [buttonStyle, setButtonStyle] = useState<string>('btn btn-primary');
+
+    useEffect(() => {
+
+        if (disabled) {
+            setButtonStyle('btn btn-primary disabled');
+        } else if (inverted) {
+            setButtonStyle('btn btn-secondary');
+        } else {
+            setButtonStyle('btn btn-primary');
+        }
+
+    }, [disabled, inverted]);
 
     return (
-        <button>
-            <h1>Button</h1>
+        <button className={buttonStyle} onClick={onClick}>
+            <p>
+                {text}
+            </p>
         </button>
     )
 }
